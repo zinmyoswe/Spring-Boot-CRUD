5c4baed8d4f040aa55a970043f7697c9417f18e5
ZIN MYO <overmidnight12@gmail.com>
2020-01-30 15:47:42 -0800

    my changes

--- src/main/java/net/codejava/ProductManager/AppController.java
+++ src/main/java/net/codejava/ProductManager/AppController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AppController {
@@ -17,6 +19,19 @@ public class AppController {
 		List<Product> listProducts = service.listAll();
 		model.addAttribute("listProducts", listProducts);
 		return "index";
+	}
+	
+	@RequestMapping("/new")
+	public String showNewProductForm(Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+		return "new_product";
+	}
+	
+	@RequestMapping(value="/save", method= RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("product") Product product) {
+		service.save(product);
+		return "redirect:/";
 	}
 	
 }
