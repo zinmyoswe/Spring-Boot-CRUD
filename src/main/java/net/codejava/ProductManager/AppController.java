bd753f9000c49f42fdc53c03923e53c523de1f65
ZIN MYO <overmidnight12@gmail.com>
2020-01-30 18:54:04 -0800

    my changes

--- src/main/java/net/codejava/ProductManager/AppController.java
+++ src/main/java/net/codejava/ProductManager/AppController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -31,6 +33,22 @@ public class AppController {
 	@RequestMapping(value="/save", method= RequestMethod.POST)
 	public String saveProduct(@ModelAttribute("product") Product product) {
 		service.save(product);
+		return "redirect:/";
+	}
+	
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditProductForm(@PathVariable(name="id") Long id) {
+		ModelAndView mav = new ModelAndView("edit_product");
+		
+		Product product = service.get(id);
+		mav.addObject("product",product);
+		
+		return mav;
+	}
+	
+	@RequestMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable(name="id") Long id) {
+		service.delete(id);
 		return "redirect:/";
 	}
 	
